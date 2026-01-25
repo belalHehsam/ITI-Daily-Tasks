69c9bd73572e8373d10ea537bf984a7879dfbf4e
Belal Hesham Osman <belalosman2410@gmail.com>
2026-01-25 23:10:36 +0200

    Delete Day2-lab directory

--- Day2-lab/js/pythagoras.js
+++
@@ -1,9 +0,0 @@
-function calc() {
-    const a = Number(document.getElementById("a").value);
-    const b = Number(document.getElementById("b").value);
-
-    const c = Math.sqrt(a * a + b * b).toFixed(2);
-
-    document.getElementById("result").textContent =
-        `c = ${c}`;
-}
